deb92909d1f51f60d6684a8a9c6903d5f38ee5c0
goo3oo <goo3oo0918@gmail.com>
2024-12-15 18:20:08 +0900

    fix: 테스트 코드 연습을 위해 서비스 계층을 인터페이스 구조로 리팩토링

---
+++ src/main/java/com/example/planner/service/PlanService.java
@@ -0,0 +1,12 @@
+package com.example.planner.service;
+
+import com.example.planner.dto.PlanRequestDto;
+import com.example.planner.dto.PlanResponseDto;
+
+import java.util.List;
+
+public interface PlanService {
+    void createPlan(PlanRequestDto requestDto);
+    List<PlanResponseDto> findAllPlan(String author, String date);
+    PlanResponseDto findPlanById(Long id);
+}
