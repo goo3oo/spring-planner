1356453afa276b3ca57fb0f9f7cfdf9351990493
goo3oo <goo3oo0918@gmail.com>
2024-12-19 16:20:45 +0900

    refactor: CommentSuccessMessage 변경                - 너무 긴 문장 간략하게 변경

--- src/main/java/com/example/planner/comment/constant/CommentSuccessMessage.java
+++ src/main/java/com/example/planner/comment/constant/CommentSuccessMessage.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommentSuccessMessage {
     POST_COMMENT_SUCCESS("댓글이 등록되었습니다."),
-    FIND_ALL_COMMENTS_BY_USER_SUCCESS("사용자가 작성한 댓글이 모두 조회되었습니다."),
+    FIND_ALL_COMMENTS_BY_USER_SUCCESS("사용자의 댓글이 모두 조회되었습니다."),
     FIND_COMMENT_BY_COMMENT_SUCCESS("댓글이 조회되었습니다."),
-    FIND_ALL_COMMENTS_BY_PLAN_SUCCESS("일정에 달린 모든 댓글이 조회되었습니다."),
+    FIND_ALL_COMMENTS_BY_PLAN_SUCCESS("일정의 댓글이 모두 조회되었습니다."),
     UPDATE_COMMENT_SUCCESS("댓글이 수정되었습니다."),
     DELETE_COMMENT_SUCCESS("댓글이 삭제되었습니다.");
 
