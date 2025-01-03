95e6ac422956fc678de9695f782494f99ffaf093
goo3oo <goo3oo0918@gmail.com>
2025-01-03 12:29:26 +0900

    Level 2-2:                                           -Modify dependencies to use service-to-service dependencies only.                                   -Return commentCount query in a single call.

--- src/main/java/com/example/planner/service/plan/PlanService.java
+++ src/main/java/com/example/planner/service/plan/PlanService.java
@@ -3,6 +3,7 @@ package com.example.planner.service.plan;
 import com.example.planner.dto.common.PageResponseDto;
 import com.example.planner.dto.plan.PlanRequestDto;
 import com.example.planner.dto.plan.PlanResponseDto;
+import com.example.planner.model.Plan;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.data.domain.Pageable;
 
@@ -11,8 +12,9 @@ import java.time.LocalDate;
 public interface PlanService {
 
     PlanResponseDto createPlan(PlanRequestDto requestDto, HttpSession session);
-    PageResponseDto findAllPlan(Long userId, LocalDate date, Pageable pageable);
     PlanResponseDto findPlanById(Long planId);
     PlanResponseDto updatePlan(Long planId, Long sessionUserId, PlanRequestDto requestDto);
     void deletePlan(Long planId, Long sessionUserId);
+    Plan findById(Long planId);
+    PageResponseDto findAllPlan(Long userId, LocalDate date, Pageable pageable);
 }
